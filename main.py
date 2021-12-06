edc811971a6200d545c1af9a3990d3b9a569a9d4
connor <chanwick@gmail.com>
2021-12-06 13:45:23 -0500

    Created login and dashboard templates

--- main.py
+++ main.py
@@ -17,7 +17,23 @@ def main_page():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    pass
+    error = None
+
+    if request.method=='POST':
+        if request.form['username'] != check_username:
+            error = 'Incorrect username'
+        elif request.form['password'] != check_password:
+            error = 'Incorrect password'
+        else:
+        #     session['logged_in'] = True 
+            return redirect(url_for('dashboard'))
+
+    return render_template('login.html', error=error)
+
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html', copy=[1,2,3,4,5])
 
 
 if __name__ == '__main__':
