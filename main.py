cbd7990fb096dc4dedd1161a255cad950f2d7c1d
connor <chanwick@gmail.com>
2021-12-06 13:16:07 -0500

    Started Login page

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-#
+from flask import Flask, session, redirect, url_for, request, render_template, current_app, g, flash
+import requests
+from bs4 import BeautifulSoup
+from random import randint
+import poem
+
+
+app = Flask(__name__)
+
+check_username = 'admin'
+check_password = 'password'
+
+
+@app.route('/')
+def main_page():
+    return redirect('/login')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    pass
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
